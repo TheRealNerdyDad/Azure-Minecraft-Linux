5dbb5c33391063ae23f1abb8f0f4ed95b1133a3e
NerdyDad <nerdydad@nerdydad.me>
2022-08-27 13:19:09 -0500

    Update azure-minecraft-linux.sh
    Added curl addresses

--- azure-minecraft-linux.sh
+++ azure-minecraft-linux.sh
@@ -3,8 +3,16 @@
 # Update apt cache.
 sudo apt-get update
 
-# Install Minecraft Bedrock software
+# Install curl & unzip
+sudo apt-get install curl -y
+sudo apt-get install unzip -y
 
+# Install Minecraft Bedrock software
+mkdir ~/minecraft
+curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.21.01.zip
+unzip -q -d ~/minecraft bedrock-server-1.19.21.01.zip
 
 # Pull down Hermicraft Server
-https://download.hermitcraft.com/hermitcraft6-bedrock.zip 
+curl https://download.hermitcraft.com/hermitcraft6-bedrock.zip
+mkdir ~/hermicraft
+unzip -q -d ~/hermicraft hermicraft6-bedrock.zip
