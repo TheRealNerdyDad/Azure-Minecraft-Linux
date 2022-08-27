a6647b382ceab1071f370a2b064f5e62c1490076
NerdyDad <nerdydad@nerdydad.me>
2022-08-27 11:20:31 -0500

    Create azure-minecraft-linux.sh
    Initial start of the file, starting initial update cache refresh, downloading and installing #Minecraft #Bedrock, and setting up #Hermicraft s7 server

---
+++ azure-minecraft-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+# Install Minecraft Bedrock software
+
+
+# Pull down Hermicraft Server
+https://download.hermitcraft.com/hermitcraft6-bedrock.zip 
